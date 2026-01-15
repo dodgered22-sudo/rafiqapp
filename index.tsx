c5c8c9d8271a8356a8b3c4c0cf5985b7c29c9afe
dodgered22-sudo <dodgered22@gmail.com>
2026-01-15 21:58:06 +0200

    Add CSS import to index.tsx
    Import index.css for styling.

--- index.tsx
+++ index.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // مهم جداً
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
